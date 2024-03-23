378404ae503e8fd110774c3406b75176e1aae517
Monika <codingbymon@gmail.com>
2024-03-23 08:26:37 +0100

    first note in js file

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const URL = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+// const promise = fetch(URL);
+// console.log(promise);
+// fetch zwraca nam zawsze promise
