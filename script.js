e451e3b69755e5cb0467def0b69923550e3925e5
Monika <codingbymon@gmail.com>
2024-03-24 14:07:14 +0100

    finish project

--- script.js
+++ script.js
@@ -17,23 +17,27 @@ function findMatches(wordToMatch, cities) {
 	})
 }
 
+function numberWithCommas(x) {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+
 function displayMatches() {
 	const matchArray = findMatches(this.value, cities)
 
-	const html = matchArray.map(place => {
+	const html = matchArray
+		.map(place => {
+			const regex = new RegExp(this.value, 'gi')
+			const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`)
+			const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`)
 
-		const regex = new RegExp(this.value, 'gi')
-		const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`)
-		const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`)
-
-		return `<li>
+			return `<li>
         <span class="name">${cityName}${stateName}</span>
-        <span class="population">${place.population}</span></li>`
-	}).join('')
+        <span class="population">${numberWithCommas(place.population)}</span></li>`
+		})
+		.join('')
 
 	suggestions.innerHTML = html
 }
 
 input.addEventListener('change', displayMatches)
 input.addEventListener('keyup', displayMatches)
-
